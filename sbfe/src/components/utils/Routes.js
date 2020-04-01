f07c0e993d6fd9bb317b88897bde1a72d2ab493c
FreeKeyBoy <jjhutton81@gmail.com>
2020-04-01 12:53:19 -0700

    Started working on notes endpoints. Debugging needed. Commented out file to prevent crashing. Added material ui boiler plate.

--- sbfe/src/components/utils/Routes.js
+++ sbfe/src/components/utils/Routes.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import { Home } from "../Home";
+// import { Home } from "../Notes";
+import { Notes } from "../Notes";
 import { Login } from "../Login"
 
 export const Routes = () => {
   return (
     <>
-      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/" component={Home} /> */}
       <Route path= "/Login" component={Login} /> 
+      <Route path= "/notes" component={Notes} />
     </>
   );
 };
