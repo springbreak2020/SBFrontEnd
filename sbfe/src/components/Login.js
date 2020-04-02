c30800140ea9204e4f4e513cb6c5bff6c34d147b
FreeKeyBoy <jjhutton81@gmail.com>
2020-04-02 13:47:18 -0700

    Login screen responsive.

--- sbfe/src/components/Login.js
+++ sbfe/src/components/Login.js
@@ -9,7 +9,7 @@ export const Login = () => {
       <h1>
         Welcome to Basic Budget Tracker. Please log in or sign up to continue.{" "}
       </h1>
-      <div class="g-signin2" data-onsuccess="onSignIn"></div>
+      <div className="g-signin2" data-onsuccess="onSignIn"></div>
     </div>
   );
 };
