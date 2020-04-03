df0fc4b7192a6752e8871c6f0a25e603b5c45d5a
isaiah francois <francoisisaiaht@yahoo.com>
2020-04-02 23:56:47 -0400

    Created Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div>
+      <p>Header</p>
+    </div>
+  );
+};
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./Login.css";
+import { Header } from "../Header/Header";
 
 export const Login = () => {
   return (
-    <div className="login-body">
-      <div className="decorative-div"></div>
-      <h1>
-        Welcome to Basic Budget Tracker. Please log in or sign up to continue.{" "}
-      </h1>
-      <div class="g-signin2" data-onsuccess="onSignIn"></div>
-    </div>
+    <>
+      <Header />
+      <div className="login-body">
+        <div className="decorative-div"></div>
+        <h1>
+          Welcome to Basic Budget Tracker. Please log in or sign up to continue.{" "}
+        </h1>
+        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+      </div>
+    </>
   );
 };
