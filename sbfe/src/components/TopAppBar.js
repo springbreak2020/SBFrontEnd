03e51f81439ce071b0879a07dc8c5fd5b323482b
FreeKeyBoy <jjhutton81@gmail.com>
2020-04-02 19:26:20 -0700

    Fixed the menu open/closed functionality.

--- sbfe/src/components/TopAppBar.js
+++ sbfe/src/components/TopAppBar.js
@@ -1,39 +1,33 @@
-import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
-import MenuExpansion from "./MenuExpansion";
-
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import Menu from "@material-ui/core/Menu";
+import React from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
 
 import "../App.css";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  }
-}));
+const options = [
+  "Income/Expense",
+  "Savings",
+  "Debt",
+  <Link to="/budgets">Budgets</Link>,
+  <Link to="/">Notes</Link>
+];
 
-export default function TopAppBar() {
-  const classes = useStyles();
+export default function SimpleListMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const [menuActive, setMenuState] = useState(false);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
 
-  const handleClick = event => {
+  const handleClickListItem = event => {
     setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    setAnchorEl(null);
   };
 
   const handleClose = () => {
@@ -41,44 +35,40 @@ export default function TopAppBar() {
   };
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-            onClick={handleClick}
+    <div>
+      <List component="nav" aria-label="Device settings">
+        <ListItem
+          button
+          aria-haspopup="true"
+          aria-controls="lock-menu"
+          //   aria-label="when device is locked"
+          onClick={handleClickListItem}
+        >
+          <MenuIcon fontSize="large" />
+          <ListItemText
+            primary="Balance $XXX.XX"
+            secondary={options[selectedIndex]}
+          />
+        </ListItem>
+      </List>
+      <Menu
+        id="lock-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {options.map((option, index) => (
+          <MenuItem
+            key={option}
+            disabled={index === 0}
+            selected={index === selectedIndex}
+            onClick={event => handleMenuItemClick(event, index)}
           >
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            //   className={` ${
-            //     menuActive ? "none" : "display"
-            //   }`}
-            //   onClick={() => setMenuState(!menuActive)}
-            >
-              <MenuItem onClick={handleClose}>
-                <MenuExpansion />
-              </MenuItem>
-              <MenuItem onClick={handleClose}>
-                <Link to="./budgets" onClick={handleClose}>
-                  Budgets
-                </Link>
-              </MenuItem>
-            </Menu>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Balance: $XXXX.XX
-          </Typography>
-          <Button variant="outlined">Logout</Button>
-        </Toolbar>
-      </AppBar>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
     </div>
   );
 }
