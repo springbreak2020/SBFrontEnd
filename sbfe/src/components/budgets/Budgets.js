374fa83d74b46e158e7b51ae0bf9aa92d1a9820a
FreeKeyBoy <jjhutton81@gmail.com>
2020-04-03 17:45:59 -0700

    Styled budgets table.

--- sbfe/src/components/budgets/Budgets.js
+++ sbfe/src/components/budgets/Budgets.js
@@ -1,26 +1,53 @@
 import React from "react";
 
-export const Budgets = () => {
-    return(
-        <div>
-            <h3>Budget Period</h3>
-            {/* link to budget Period */}
-            <h3>Start Date</h3>
-            {/* link to start Date */}
-            <h3>End Date</h3>
-            {/* link to end date */}
-            <h3>Income and Expenses</h3>
-            {/* link to categories used in budget Period */}
-            <h3>Income Total</h3>
-            {/* link to income Total */}
-            <h3>Expense Total</h3>
-            {/* link to Expense Total */}
-            <h3>Period Total</h3>
-            {/* link to period total */}
-        </div>
-    )
-} 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
 
-// All of the fields on this table populate from other fields and TableS. 
-// None of it can be edited from this screen. 
+import Sidebar from "../sidebar/Sidebar";
+
+import "./budgets.css";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    height: "fit-content",
+    margin: "10px auto",
+  },
+  text: {
+    textAlign: "left",
+    margin: "0 auto",
+    width: "90%",
+  },
+});
+
+export const Budgets = () => {
+  const classes = useStyles();
+  return (
+    <div className="budget-body">
+      <Sidebar />
+      <Card className={classes.root} variant="outlined">
+        <Typography variant="body2" className={classes.text}>
+          <h3>Budget Period</h3>
+          {/* link to budget Period */}
+          <h3>Start Date</h3>
+          {/* link to start Date */}
+          <h3>End Date</h3>
+          {/* link to end date */}
+          <h3>Income and Expenses</h3>
+          {/* link to categories used in budget Period */}
+          <h3>Income Total</h3>
+          {/* link to income Total */}
+          <h3>Expense Total</h3>
+          {/* link to Expense Total */}
+          <h3>Period Total</h3>
+          {/* link to period total */}
+        </Typography>
+      </Card>
+    </div>
+  );
+};
+
+// All of the fields on this table populate from other fields and TableS.
+// None of it can be edited from this screen.
 // So we'll need to create endpoints to get the data, then map it onto the component
