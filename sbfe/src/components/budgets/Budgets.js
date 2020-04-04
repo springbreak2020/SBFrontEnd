a88cd60103a3e082bf8f3a788a32371e4410fefb
FreeKeyBoy <jjhutton81@gmail.com>
2020-04-03 21:23:14 -0700

    Added top/bottom bars to budgets.

--- sbfe/src/components/budgets/Budgets.js
+++ sbfe/src/components/budgets/Budgets.js
@@ -5,14 +5,19 @@ import Card from "@material-ui/core/Card";
 import Typography from "@material-ui/core/Typography";
 
 import Sidebar from "../sidebar/Sidebar";
+import TopBar from "../topbar/TopBar";
+import BottomBar from "../bottombar/BottomBar";
 
 import "./budgets.css";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-    height: "fit-content",
     margin: "10px auto",
+    height: "fit-content",
+    justifyContent: "center",
+    width: "90%",
+    maxWidth: "400px",
   },
   text: {
     textAlign: "left",
@@ -26,24 +31,28 @@ export const Budgets = () => {
   return (
     <div className="budget-body">
       <Sidebar />
-      <Card className={classes.root} variant="outlined">
-        <Typography variant="body2" className={classes.text}>
-          <h3>Budget Period</h3>
-          {/* link to budget Period */}
-          <h3>Start Date</h3>
-          {/* link to start Date */}
-          <h3>End Date</h3>
-          {/* link to end date */}
-          <h3>Income and Expenses</h3>
-          {/* link to categories used in budget Period */}
-          <h3>Income Total</h3>
-          {/* link to income Total */}
-          <h3>Expense Total</h3>
-          {/* link to Expense Total */}
-          <h3>Period Total</h3>
-          {/* link to period total */}
-        </Typography>
-      </Card>
+      <div className="topbar-card-wrapper">
+        <TopBar />
+        <Card className={classes.root} variant="outlined">
+          <Typography variant="body2" className={classes.text}>
+            <h3>Budget Period</h3>
+            {/* link to budget Period */}
+            <h3>Start Date</h3>
+            {/* link to start Date */}
+            <h3>End Date</h3>
+            {/* link to end date */}
+            <h3>Income and Expenses</h3>
+            {/* link to categories used in budget Period */}
+            <h3>Income Total</h3>
+            {/* link to income Total */}
+            <h3>Expense Total</h3>
+            {/* link to Expense Total */}
+            <h3>Period Total</h3>
+            {/* link to period total */}
+          </Typography>
+        </Card>
+        <BottomBar />
+      </div>
     </div>
   );
 };
