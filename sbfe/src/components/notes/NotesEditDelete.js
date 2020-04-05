db7632f40218bfe925908986a73cdbfac7864b4c
FreeKeyBoy <jjhutton81@gmail.com>
2020-04-04 20:24:39 -0700

    Completed all edit/delete cards.

--- sbfe/src/components/notes/NotesEditDelete.js
+++ sbfe/src/components/notes/NotesEditDelete.js
@@ -1,58 +1,73 @@
 import React from "react";
 
-import Sidebar from "../sidebar/Sidebar";
-
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 
-import "./notes.css";
+import Sidebar from "../sidebar/Sidebar";
+import TopBar from "../topbar/TopBar";
+import BottomBar from "../bottombar/BottomBar";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-    justifyContent: "center",
     margin: "10px auto",
-    maxWidth: "325px",
-    width: "100%",
     height: "fit-content",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    width: "90%",
+    maxWidth: "400px",
+  },
+  text: {
+    borderBottom: "1px solid grey",
+    textAlign: "center",
+    margin: "10px auto",
+    width: "90%",
   },
   input: {
-    width: "90%",
     margin: "10px auto",
+    width: "90%",
+  },
+  buttonWrap: {
     display: "flex",
-    padding: "1%"
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
 export const NotesEditDelete = (props) => {
   const classes = useStyles();
+  const [value, setValue] = React.useState("");
   const toNotes = () => {
     props.history.push("/dashboard");
   };
   return (
-    <div className="note-edit-body">
+    <div style={{ display: "flex" }}>
       <Sidebar />
-      <Card className={classes.root} variant="outlined">
-        <TextField
-          id="filled-basic"
-          label="Any thoughts you'd like to record about this entry?"
-          variant="filled"
-          fullWidth
-          multiline
-          margin="normal"
-          className={classes.input}
-        />
-        <CardActions className={classes.root}>
-          <Button size="medium">Save</Button>
-          <Button size="medium" onClick={toNotes}>
-            Cancel
-          </Button>
-        </CardActions>
-      </Card>
+      <div style={{ width: "100%" }}>
+        <TopBar />
+        <Card className={classes.root} variant="outlined">
+          <TextField
+            id="filled-basic"
+            label="Anything you'd like to record?"
+            variant="filled"
+            multiline
+            className={classes.input}
+            onChange={(e, value) => setValue(value)}
+          />
+          <CardActions className={classes.buttonWrap}>
+            <Button size="medium" onClick={toNotes}>
+              Save
+            </Button>
+            <Button size="medium" onClick={toNotes}>
+              Cancel
+            </Button>
+          </CardActions>
+        </Card>
+        <BottomBar />
+      </div>
     </div>
   );
 };
